778f62d3a2026614990705d6ad8e4c4355a5c0b3
Adam Cross <adamfgcross@gmail.com>
2023-11-22 11:22:10 -0500

    cleanup DictionaryImpl

--- src/main/java/com/adamfgcross/randomwords/resourcelayer/DictionaryImpl.java
+++ src/main/java/com/adamfgcross/randomwords/resourcelayer/DictionaryImpl.java
@@ -1,7 +1,6 @@
 package com.adamfgcross.randomwords.resourcelayer;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -27,6 +26,7 @@ public class DictionaryImpl implements Dictionary {
 			return this.wordList.size();
 		}
 
+		@SuppressWarnings("unused")
 		private void init() throws Exception {
 			wordList = new ArrayList<String>();
 			try (BufferedReader reader = new BufferedReader(new InputStreamReader(wordListAddress.getInputStream()))) {
