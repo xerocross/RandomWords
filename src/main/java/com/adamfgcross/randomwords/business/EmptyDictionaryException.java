66f0d1ce4c81f977312a75dd1d3232c164f4a726
Adam Cross <adamfgcross@gmail.com>
2023-11-22 11:21:33 -0500

    add serialVersionUID to EmptyDictionaryException

--- src/main/java/com/adamfgcross/randomwords/business/EmptyDictionaryException.java
+++ src/main/java/com/adamfgcross/randomwords/business/EmptyDictionaryException.java
@@ -1,4 +1,6 @@
 package com.adamfgcross.randomwords.business;
 
 public class EmptyDictionaryException extends RuntimeException {
+
+	private static final long serialVersionUID = 2992982141440281211L;
 }
